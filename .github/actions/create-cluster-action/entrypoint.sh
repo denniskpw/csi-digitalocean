b42138df97c4f9672bb3848b91c312df6982c328
Dennis Wong <dwong@digitalocean.com>
2019-09-26 14:33:01 -0400

    Create a k8s cluster

--- .github/actions/create-cluster-action/entrypoint.sh
+++ .github/actions/create-cluster-action/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -l
 
-echo "Creating droplet"
-/app/doctl compute droplet create foobar --region nyc3 --image ubuntu-18-04-x64 --size s-1vcpu-1gb
+
+# echo "Creating droplet"
+# /app/doctl compute droplet create foobar --region nyc3 --image ubuntu-18-04-x64 --size s-1vcpu-1gb
+
+NAME=test-cluster
+
+echo "Creating k8s cluster: ${NAME}"
+/app/doctl kubernetes cluster create ${NAME}
+/app/doctl kubernetes cluster kubeconfig save ${NAME}
