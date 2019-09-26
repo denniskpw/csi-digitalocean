6f8a51106d3f0e8c82c879a1021d744eb3aa12c6
Dennis Wong <dwong@digitalocean.com>
2019-09-26 16:03:43 -0400

    Reduce random string length for worker pool name limited to 55 characters

--- .github/actions/create-cluster-action/entrypoint.sh
+++ .github/actions/create-cluster-action/entrypoint.sh
@@ -4,7 +4,7 @@
 # echo "Creating droplet"
 # /app/doctl compute droplet create foobar --region nyc3 --image ubuntu-18-04-x64 --size s-1vcpu-1gb
 
-NAME=test-cluster-$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random)
+NAME=test-cluster-$(hexdump -n 12 -e '3/4 "%08X" 1 "\n"' /dev/random)
 
 echo "Creating k8s cluster: ${NAME}"
 /app/doctl kubernetes cluster create ${NAME}
